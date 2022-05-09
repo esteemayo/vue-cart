8a8c277f6b1af31824a6072b38b2b870c0517342
Emmanuel Adebayo <eadebayo15@gmail.com>
2022-05-09 06:22:32 +0100

    first commit

---
+++ src/components/Modal.vue
@@ -0,0 +1,33 @@
+<template>
+  <aside className="modal-container">
+    <div className="modal">
+      <h4>remove all items from your shopping cart?</h4>
+      <div className="btn-container">
+        <button
+          type="button"
+          className="btn confirm-btn"
+          @click="
+            clearCart();
+            closeModal();
+          "
+        >
+          confirm
+        </button>
+        <button type="button" className="btn clear-btn" @click="closeModal">
+          cancel
+        </button>
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script>
+import { mapMutations } from 'vuex';
+
+export default {
+  name: 'Modal',
+  methods: mapMutations(['closeModal', 'clearCart']),
+};
+</script>
+
+<style></style>
